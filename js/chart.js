8a4267213f230d09766d3c5b5697a1ad3e05ebb7
M Faizan Khan <59408320+Faizankhan147@users.noreply.github.com>
2025-01-28 15:42:14 +0500

    upoad all project files

---
+++ js/chart.js
@@ -0,0 +1,356 @@
+// ------- Product Chart -----------
+
+// Data retrieved from https://fas.org/issues/nuclear-weapons/status-world-nuclear-forces/
+Highcharts.chart('productChart', {
+    chart: {
+        type: 'area'
+    },
+    accessibility: {
+        description: 'Image description: An area chart compares the nuclear ' +
+            'stockpiles of the USA and the USSR/Russia between 1945 and ' +
+            '2024. The number of nuclear weapons is plotted on the Y-axis ' +
+            'and the years on the X-axis. The chart is interactive, and the ' +
+            'year-on-year stockpile levels can be traced for each country. ' +
+            'The US has a stockpile of 2 nuclear weapons at the dawn of the ' +
+            'nuclear age in 1945. This number has gradually increased to 170 ' +
+            'by 1949 when the USSR enters the arms race with one weapon. At ' +
+            'this point, the US starts to rapidly build its stockpile ' +
+            'culminating in 31,255 warheads by 1966 compared to the USSR’s 8,' +
+            '400. From this peak in 1967, the US stockpile gradually ' +
+            'decreases as the USSR’s stockpile expands. By 1978 the USSR has ' +
+            'closed the nuclear gap at 25,393. The USSR stockpile continues ' +
+            'to grow until it reaches a peak of 40,159 in 1986 compared to ' +
+            'the US arsenal of 24,401. From 1986, the nuclear stockpiles of ' +
+            'both countries start to fall. By 2000, the numbers have fallen ' +
+            'to 10,577 and 12,188 for the US and Russia, respectively. The ' +
+            'decreases continue slowly after plateauing in the 2010s, and in ' +
+            '2024 the US has 3,708 weapons compared to Russia’s 4,380.'
+    },
+    title: {
+        text: ''
+    },
+    subtitle: {
+        text: ''
+    },
+    xAxis: {
+        allowDecimals: false,
+        accessibility: {
+            rangeDescription: 'Range: 1940 to 2024.'
+        }
+    },
+    yAxis: {
+        title: {
+            text: 'Nuclear weapon states'
+        }
+    },
+    tooltip: {
+        pointFormat: '{series.name} had stockpiled <b>{point.y:,.0f}</b><br/>' +
+            'warheads in {point.x}'
+    },
+    plotOptions: {
+        area: {
+            pointStart: 1940,
+            marker: {
+                enabled: false,
+                symbol: 'circle',
+                radius: 2,
+                states: {
+                    hover: {
+                        enabled: true
+                    }
+                }
+            }
+        }
+    },
+    series: [{
+        name: 'Sales',
+        data: [
+            null, null, null, null, null, 2, 9, 13, 50, 170, 299, 438, 841,
+            1169, 1703, 2422, 3692, 5543, 7345, 12298, 18638, 22229, 25540,
+            28133, 29463, 31139, 31175, 31255, 29561, 27552, 26008, 25830,
+            26516, 27835, 28537, 27519, 25914, 25542, 24418, 24138, 24104,
+            23208, 22886, 23305, 23459, 23368, 23317, 23575, 23205, 22217,
+            21392, 19008, 13708, 11511, 10979, 10904, 11011, 10903, 10732,
+            10685, 10577, 10526, 10457, 10027, 8570, 8360, 7853, 5709, 5273,
+            5113, 5066, 4897, 4881, 4804, 4717, 4571, 4018, 3822, 3785, 3805,
+            3750, 3708, 3708, 3708, 3708
+        ],
+        color: "#FF8F6DCC"
+
+    },
+    
+
+    
+    
+    {
+        name: 'Profit',
+        data: [
+            null, null, null, null, null, null, null, null, null,
+            1, 5, 25, 50, 120, 150, 200, 426, 660, 863, 1048, 1627, 2492,
+            3346, 4259, 5242, 6144, 7091, 8400, 9490, 10671, 11736, 13279,
+            14600, 15878, 17286, 19235, 22165, 24281, 26169, 28258, 30665,
+            32146, 33486, 35130, 36825, 38582, 40159, 38107, 36538, 35078,
+            32980, 29154, 26734, 24403, 21339, 18179, 15942, 15442, 14368,
+            13188, 12188, 11152, 10114, 9076, 8038, 7000, 6643, 6286, 5929,
+            5527, 5215, 4858, 4750, 4650, 4600, 4500, 4490, 4300, 4350, 4330,
+            4310, 4495, 4477, 4489, 4380
+        ] , 
+        color : "#DBA5FF",
+    }]
+});
+
+
+// ------ Customer Graph -------
+
+Highcharts.chart('customer', {
+    chart: {
+        type: 'pie',
+        custom: {},
+        events: {
+            render() {
+                const chart = this,
+                    series = chart.series[0];
+                let customLabel = chart.options.chart.custom.label;
+
+                if (!customLabel) {
+                    customLabel = chart.options.chart.custom.label =
+                        chart.renderer.label(
+                            'Total<br/>' +
+                            '<strong>2 877 820</strong>'
+                        )
+                            .css({
+                                color: '#000',
+                                textAnchor: 'middle'
+                            })
+                            .add();
+                }
+
+                const x = series.center[0] + chart.plotLeft,
+                    y = series.center[1] + chart.plotTop -
+                    (customLabel.attr('height') / 2);
+
+                customLabel.attr({
+                    x,
+                    y
+                });
+                // Set font size based on chart diameter
+                customLabel.css({
+                    fontSize: `${series.center[2] / 12}px`
+                });
+            }
+        }
+    },
+    accessibility: {
+        point: {
+            valueSuffix: '%'
+        }
+    },
+    title: {
+        text: '2023 Norway car registrations'
+    },
+    subtitle: {
+        text: 'Source: <a href="https://www.ssb.no/transport-og-reiseliv/faktaside/bil-og-transport">SSB</a>'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.0f}%</b>'
+    },
+    legend: {
+        enabled: false
+    },
+    plotOptions: {
+        series: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            borderRadius: 8,
+            dataLabels: [{
+                enabled: true,
+                distance: 20,
+                format: '{point.name}'
+            }, {
+                enabled: true,
+                distance: -15,
+                format: '{point.percentage:.0f}%',
+                style: {
+                    fontSize: '0.9em'
+                }
+            }],
+            showInLegend: true
+        }
+    },
+    series: [{
+        name: 'Registrations',
+        colorByPoint: true,
+        innerSize: '75%',
+        data: [{
+            name: 'EV',
+            y: 23.9
+        }, {
+            name: 'Hybrids',
+            y: 12.6
+        }, {
+            name: 'Diesel',
+            y: 37.0
+        }, {
+            name: 'Petrol',
+            y: 26.4
+        }]
+    }]
+});
+
+
+// ------ Sales -------
+
+
+Highcharts.chart('sales', {
+    chart: {
+        type: 'spline',
+        inverted: true
+    },
+    title: {
+        text: 'Atmosphere Temperature by Altitude'
+    },
+    subtitle: {
+        text: 'According to the Standard Atmosphere Model'
+    },
+    xAxis: {
+        reversed: false,
+        title: {
+            enabled: true,
+            text: 'Altitude'
+        },
+        labels: {
+            format: '{value} km'
+        },
+        accessibility: {
+            rangeDescription: 'Range: 0 to 80 km.'
+        },
+        maxPadding: 0.05,
+        showLastLabel: true
+    },
+    yAxis: {
+        title: {
+            text: 'Temperature'
+        },
+        labels: {
+            format: '{value}°'
+        },
+        accessibility: {
+            rangeDescription: 'Range: -90°C to 20°C.'
+        },
+        lineWidth: 2
+    },
+    legend: {
+        enabled: false
+    },
+    tooltip: {
+        headerFormat: '<b>{series.name}</b><br/>',
+        pointFormat: '{point.x} km: {point.y}°C'
+    },
+    plotOptions: {
+        spline: {
+            marker: {
+                enable: false
+            }
+        }
+    },
+    series: [{
+        name: 'Temperature',
+        data: [
+            [0, 15], [10, -50], [20, -56.5], [30, -46.5], [40, -22.1],
+            [50, -2.5], [60, -27.7], [70, -55.7], [80, -76.5]
+        ]
+
+    }]
+});
+
+
+// ------ Featured -----
+
+
+Highcharts.chart('featured', {
+    title: {
+        text: 'Growth of Internet Users Worldwide (logarithmic scale)'
+    },
+
+    accessibility: {
+        point: {
+            valueDescriptionFormat:
+                '{xDescription}{separator}{value} million(s)'
+        }
+    },
+
+    xAxis: {
+        title: {
+            text: 'Year'
+        },
+        categories: [1995, 2000, 2005, 2010, 2015, 2020, 2023]
+    },
+
+    yAxis: {
+        type: 'logarithmic',
+        title: {
+            text: 'Number of Internet Users (in millions)'
+        }
+    },
+
+    tooltip: {
+        headerFormat: '<b>{series.name}</b><br />',
+        pointFormat: '{point.y} million(s)'
+    },
+
+    series: [{
+        name: 'Internet Users',
+        keys: ['y', 'color'],
+        data: [
+            [16, '#0000ff'],
+            [361, '#8d0073'],
+            [1018, '#ba0046'],
+            [2025, '#d60028'],
+            [3192, '#eb0014'],
+            [4673, '#fb0004'],
+            [5200, '#ff0000']
+        ],
+        color: {
+            linearGradient: {
+                x1: 0,
+                x2: 0,
+                y1: 1,
+                y2: 0
+            },
+            stops: [
+                [0, '#0000ff'],
+                [1, '#ff0000']
+            ]
+        }
+    }]
+});
+
+
+// ------- Dashboard Graph --------
+
+
+// (async () => {
+
+//     const data = await fetch(
+//         'https://demo-live-data.highcharts.com/aapl-c.json'
+//     ).then(response => response.json());
+
+//     // Create the chart
+//     Highcharts.stockChart('container', {
+//         rangeSelector: {
+//             selected: 1
+//         },
+
+//         title: {
+//             text: 'AAPL Stock Price'
+//         },
+
+//         series: [{
+//             name: 'AAPL',
+//             data: data,
+//             tooltip: {
+//                 valueDecimals: 2
+//             }
+//         }]
+//     });
+// })();
---
+++ js/table.js
@@ -0,0 +1,3 @@
+// ------ Data Tables ------
+
+new DataTable('#example');
